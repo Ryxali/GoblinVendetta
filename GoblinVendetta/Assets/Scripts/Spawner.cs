c002e733d0d2f4c3ec63f52f5e0911598dfc55a5
Sebbish <sebastian.jarkeborn@gmail.com>
2014-11-29 19:04:06 +0100

    spawner

---
+++ GoblinVendetta/Assets/Scripts/Spawner.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour {
+	int lastspawn;
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Time.time > lastspawn + 2) {
+
+		}
+	}
+}
