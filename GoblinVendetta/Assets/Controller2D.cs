817da62c5ebe9caf914d8f6926b224b5f244543a
Ryxali <ryxali@gmail.com>
2014-11-29 16:04:15 +0100

    Added controller

--- GoblinVendetta/Assets/Controller2D.cs
+++ GoblinVendetta/Assets/Controller2D.cs
@@ -1,11 +1,22 @@
-﻿using UnityEngine;
+﻿ 	using UnityEngine;
 using System.Collections;
 
 public class Controller2D : MonoBehaviour {
 
 	private Transform character;
 	private Vector2 dir;
-	public float speed;
+	// Current target
+	private Vector3 target;
+	// Current velocity
+	private Vector3 vel;
+
+	// Acceleration
+	public float speed = 5;
+	// Terminal leg-providied velocity
+	public float maxSpeed = 10;
+	public float jumpForce = 10;
+
+	public FeetCollider feet;
 
 	// Use this for initialization
 	void Awake () {
@@ -14,11 +25,29 @@ public class Controller2D : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		
+		Vector3 curVel = new Vector3 ();
+		curVel.x = character.rigidbody2D.velocity.x;
+		curVel.y = character.rigidbody2D.velocity.y;
+
 		dir.x = Input.GetAxis ("Horizontal");
 		dir.y = Input.GetAxis ("Vertical");
-		float x = character.position.x + dir.x * Time.deltaTime * speed;
+
 		Vector3 t = new Vector3 ();
-		t.x = x;
-		character.position = t;
+		t.x = dir.x * speed * Time.deltaTime + curVel.x;
+		curVel.x = 0;
+
+		if (t.magnitude > maxSpeed) {
+			t = t.normalized * maxSpeed;
+		}
+		//character.position = t;
+		if (feet.isGrounded && Input.GetButton ("Jump")) {
+			//character.rigidbody2D.AddRelativeForce(new Vector2(0, jumpForce));
+			curVel.y = jumpForce;
+		}
+
+		character.rigidbody2D.velocity = curVel + t;
+
+
 	}
 }
---
+++ GoblinVendetta/Assets/FeetCollider.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FeetCollider : MonoBehaviour {
+
+	public Rigidbody2D body;
+
+	public bool isGrounded { get; private set; }
+	private bool feetTouching = false;
+	void Update() {
+		isGrounded = feetTouching && body.velocity.y == 0;
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		feetTouching = true;
+	}
+
+	void OnTriggerExit2D(Collider2D other) {
+		feetTouching = false;
+	}
+}
