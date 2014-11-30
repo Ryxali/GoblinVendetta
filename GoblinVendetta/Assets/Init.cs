af78a9fbece99496404b5345d4f4f72ddb4878f9
Ryxali <ryxali@gmail.com>
2014-11-30 19:59:42 +0100

    sadjsaoöfui

---
+++ GoblinVendetta/Assets/Init.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Init : MonoBehaviour {
+
+	public GameObject level;
+	public GameObject everything;
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown ("Fire1")) {
+			everything.SetActive(true);
+			level.SetActive (true);
+			gameObject.SetActive(false);
+		}
+
+	}
+}
---
+++ GoblinVendetta/Assets/crosshair.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class crosshair : MonoBehaviour {
+	
+	void Awake()
+	{
+
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Screen.showCursor = false;
+		Vector3 mPos = GlobalVariables.vars.cam.ScreenToWorldPoint(Input.mousePosition);
+		mPos.z = 0;
+		transform.position = mPos;
+	}
+}
