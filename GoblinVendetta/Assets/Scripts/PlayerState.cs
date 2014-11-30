9c1be8f66ed2b86a835b2fc1078357d47abdd5ea
ViktorZryd <viktorzryd@gmail.com>
2014-11-30 01:54:23 +0100

    Fixat everything
    lsaolasdl

--- GoblinVendetta/Assets/Scripts/PlayerState.cs
+++ GoblinVendetta/Assets/Scripts/PlayerState.cs
@@ -1,8 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+	
+
 public class PlayerState : Hitpoints {
 	public GoblinStats stats = new GoblinStats(1,1,1,1,1,"lol");
+	public AudioClip[] DamageSound;
+	public AudioClip[] DeathSound;
 
 	public void SetStats (GoblinStats s)
 	{
@@ -14,7 +18,14 @@ public class PlayerState : Hitpoints {
 		return stats;
 	}
 
+	public override void Hit (int damage)
+	{
+		PlayDamageSound ();
+		base.Hit (damage);
+	}
+
 	public override void Die() {
+		audio.PlayOneShot(DeathSound[Random.Range(0,DeathSound.Length)]);
 		GlobalVariables.vars.playerShouldRespawn = true;
 	}
 
@@ -27,4 +38,9 @@ public class PlayerState : Hitpoints {
 	void Update () {
 	
 	}
+	void PlayDamageSound()
+	{
+		audio.PlayOneShot(DamageSound[Random.Range(0,DamageSound.Length)]);
+	}
+	
 }
